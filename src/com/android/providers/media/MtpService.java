7eeb06359452e3f6a9885f9737b10fe7546d86f9
Bo Huang <bo.b.huang@intel.com>
2013-01-23 14:06:40 -0500

    MtpService: start MtpServer if it's not alive
    Rootcause:
    When trying to start MtpServer, if MtpServer is not NULL but
    is dead, should start the thread again.
    
    Dependency:
    Depend on MtpServer under /frameworks/base
    https://android-review.googlesource.com/#/c/117751/
    
    Reproduced steps:
    pre-condition:
    DUT with userdebug version;
    DUT enable Encryption and Screen lock;
    
    Test step:
    1. power on DUT connected with PC;
    2. run "adb wait-for-device; adb root" from PC;
    3. unlock DUT;
    
    Expected result:
    DUT should display on PC.
    
    Test result:
    Nothing showed on PC.
    
    Error log:
    01-01 00:00:15.240 D/MtpDeviceJNI(  309): register_android_mtp_MtpDevice
    01-01 00:00:34.180 D/Mtp_UsbDeviceManager(  579): systemReady
    01-01 00:00:34.200 D/Mtp_UsbDeviceManager(  579): setAdbEnabled: true
    01-01 00:00:34.300 D/Mtp_UsbDeviceManager(  579): broadcasting Intent { act=android.hardware.usb.action.USB_STATE flg=0x30000000 (has extras) } connected: true configured: tr
    01-01 00:00:38.310 D/Mtp_UsbDeviceManager(  579): boot completed
    [   41.472761] init: untracked pid 1266 killed by signal 9
    [   41.491408] init: untracked pid 1246 killed by signal 9
    [   41.494739] init: untracked pid 1128 killed by signal 9
    [   41.507095] init: untracked pid 1285 killed by signal 9
    [   41.512010] init: untracked pid 1347 killed by signal 9
    [   41.520560] init: untracked pid 960 killed by signal 9
    [   42.777285] init: untracked pid 579 killed by signal 9
    12-31 19:00:47.851 I/KERNEL  (    1): [   43.660523] mtp_bind_config
    12-31 19:00:49.810 D/MtpDeviceJNI( 1544): register_android_mtp_MtpDevice
    12-31 19:01:04.510 D/Mtp_UsbDeviceManager( 1844): systemReady
    12-31 19:01:04.610 D/Mtp_UsbDeviceManager( 1844): broadcasting Intent { act=android.hardware.usb.action.USB_STATE flg=0x30000000 (has extras) } connected: true configured: tr
    12-31 19:01:04.740 I/ActivityManager( 1844): Start proc android.process.media for broadcast com.android.providers.media/.MtpReceiver: pid=2013 uid=10007 gids={50007, 9997, 20
    12-31 19:01:04.800 V/BroadcastQueue( 1844): Delivering to component ComponentInfo{com.android.providers.media/com.android.providers.media.MtpReceiver}: BroadcastRecord{2d4f6d
    12-31 19:01:05.030 D/Mtp_Service( 2013): updating state; isCurrentUser=true, mMtpLocked=true
    12-31 19:01:05.060 D/Mtp_Service( 2013): updating state; isCurrentUser=true, mMtpLocked=true
    12-31 19:01:05.090 D/Mtp_Service( 2013): starting MTP server in MTP mode
    12-31 19:01:05.093 I/KERNEL  ( 2013): [   60.897833] mtp_open
    12-31 19:01:06.781 I/KERNEL  ( 2013): [   62.584738] mtp_release
    12-31 19:01:06.844 I/KERNEL  ( 2409): [   62.648100] mtp_bind_config
    12-31 19:01:07.270 V/Mtp_UsbDeviceManager( 1844): USB UEVENT: {USB_STATE=DISCONNECTED, SUBSYSTEM=android_usb, SEQNUM=1853, ACTION=change, DEVPATH=/devices/virtual/android_usb
    12-31 19:01:07.300 V/Mtp_UsbDeviceManager( 1844): USB UEVENT: {USB_STATE=CONNECTED, SUBSYSTEM=android_usb, SEQNUM=1854, ACTION=change, DEVPATH=/devices/virtual/android_usb/an
    12-31 19:01:07.340 V/Mtp_UsbDeviceManager( 1844): USB UEVENT: {USB_STATE=CONFIGURED, SUBSYSTEM=android_usb, SEQNUM=1855, ACTION=change, DEVPATH=/devices/virtual/android_usb/a
    12-31 19:01:08.210 E/MtpServerJNI( 2013): server is null in send_object_removed
    12-31 19:01:14.340 D/Mtp_Service( 2013): addStorageLocked 65537 /storage/emulated/0
    12-31 19:01:14.390 E/MtpServerJNI( 2013): server is null in add_storage
    
    Change-Id: I1ca5243e30e202aee901b99ff0cabfed51ccec6b
    Signed-off-by: jzhuan5 <jin.can.zhuang@intel.com>
    Signed-off-by: Bo Huang <bo.b.huang@intel.com>

--- src/com/android/providers/media/MtpService.java
+++ src/com/android/providers/media/MtpService.java
@@ -186,7 +186,10 @@ public class MtpService extends Service {
      */
     private void manageServiceLocked() {
         final boolean isCurrentUser = UserHandle.myUserId() == ActivityManager.getCurrentUser();
-        if (mServer == null && isCurrentUser) {
+        if ((mServer == null || !mServer.isAlive()) && isCurrentUser) {
+            if (mServer != null)
+                Log.d(TAG, "MTP Server is not running");
+
             Log.d(TAG, "starting MTP server in " + (mPtpMode ? "PTP mode" : "MTP mode"));
             mServer = new MtpServer(mDatabase, mPtpMode);
             mDatabase.setServer(mServer);
