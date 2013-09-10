a322b8a11415d2073161a5ec04f5aea8786bcfd6
Antonius Harijanto <aharijanto@nvidia.com>
2013-09-10 16:53:03 -0700

    MediaScanner to update its library after SD card dismounted.
    Android has a library that keeps track of media files on the device.
    This library needs to be updated when SD card is dismounted.
    
    Change-Id: Ica530819c5616a53448fb1554ee816c7500b75ac

--- src/com/android/providers/media/MediaScannerReceiver.java
+++ src/com/android/providers/media/MediaScannerReceiver.java
@@ -2,16 +2,16 @@
 **
 ** Copyright 2007, The Android Open Source Project
 **
-** Licensed under the Apache License, Version 2.0 (the "License"); 
-** you may not use this file except in compliance with the License. 
-** You may obtain a copy of the License at 
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
 **
-**     http://www.apache.org/licenses/LICENSE-2.0 
+**     http://www.apache.org/licenses/LICENSE-2.0
 **
-** Unless required by applicable law or agreed to in writing, software 
-** distributed under the License is distributed on an "AS IS" BASIS, 
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-** See the License for the specific language governing permissions and 
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
 
@@ -58,7 +58,8 @@ public class MediaScannerReceiver extends BroadcastReceiver {
                 }
 
                 Log.d(TAG, "action: " + action + " path: " + path);
-                if (Intent.ACTION_MEDIA_MOUNTED.equals(action)) {
+                if (Intent.ACTION_MEDIA_MOUNTED.equals(action)
+                    || Intent.ACTION_MEDIA_UNMOUNTED.equals(action)) {
                     // scan whenever any volume is mounted
                     scan(context, MediaProvider.EXTERNAL_VOLUME);
                 } else if (Intent.ACTION_MEDIA_SCANNER_SCAN_FILE.equals(action) &&
@@ -74,12 +75,12 @@ public class MediaScannerReceiver extends BroadcastReceiver {
         args.putString("volume", volume);
         context.startService(
                 new Intent(context, MediaScannerService.class).putExtras(args));
-    }    
+    }
 
     private void scanFile(Context context, String path) {
         Bundle args = new Bundle();
         args.putString("filepath", path);
         context.startService(
                 new Intent(context, MediaScannerService.class).putExtras(args));
-    }    
+    }
 }
