3e60dae7f284253c5b13aaa94849c7b644ffd4f5
Jerry Zhang <zhangjerry@google.com>
2016-12-13 17:02:32 -0800

    Add tests for MediaProvider permissions.
    Automated test checks for surfaceflinger, mtp permissions.
    
    Test: Run tests through adb
    Bug: 33574909
    Change-Id: I0aa64d61320a03933fca8f18a26577f7e95de2e1

--- Android.mk
+++ Android.mk
@@ -3,13 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files) \
+LOCAL_SRC_FILES := $(call all-java-files-under,src) \
 	src/com/android/providers/media/IMtpService.aidl
 
 LOCAL_JAVA_LIBRARIES := 
 
 LOCAL_PACKAGE_NAME := MediaProvider
+
 LOCAL_CERTIFICATE := media
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
